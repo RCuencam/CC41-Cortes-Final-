a3263a97d985c34c6e27f0f821b0c50d5bb4d61f
Rodrigo <u201710092@upc.edu.pe>
2019-11-17 20:11:49 -0500

    avance+

--- BruteForce.py
+++ BruteForce.py
@@ -1,4 +1,4 @@
-
+import heapq as hq
 
 def AreaTotal(resultado):
     n=len(resultado)
@@ -59,7 +59,7 @@ def SepararArea(arreglo):
 	
 	return a
 
-#
+
 def imprimir():
 	print("Area,ancho,alto")
 	print(MostrarAreas(G))
@@ -79,9 +79,6 @@ def imprimir():
 	print(AreaTotal(volumen))
 	print(Desperdicio(AreaTotal(volumen)),"%")
 
-
-def posicionar(x,y,z,arreglo):
-    n=len(x)
 
 with open('archivo.txt') as f:
     
@@ -107,6 +104,7 @@ with open('archivo.txt') as f:
             CL.append(int(Y))
             CL.append(int(Z))
             areas.append(int(X)*int(Y)*int(Z))
+        
         
 
 def areaTotal(areas):
@@ -137,23 +135,110 @@ for i in range(n):
     for j in range(3):
         resultado.append(a[i][j])
 
-print(ordenado)
-print(resultado)
 
-def localizar(arreglo):
-
-    temporal=[]
+'''def posicionar(largo,ancho,alto,arreglo,ordenado):
+    n=len(arreglo)
+    resultado=[]
+    resultado.append([0,0,0])
     x=0
     y=0
+    z=arreglo[2]
+    q=0
+    q1=0
+    q2=0
+    z1=0
+    #while x<largo and y<ancho and z<alto:# Para que no sobrepase las dimensiones de la caja
+    cambio=False
+    
+
+    while z<=alto and q<4:
+        
+        resultado.append([0,0,z])
+        z+=arreglo[2]
+        q+=1
+        a=ordenado[1]
+    while z1<=alto and q1<2:
+
+        resultado.append([0,a[1],z1])
+        z1+=arreglo[2]
+        q1+=1
+    
+    while z1<=alto and q2<6:
+        
+        a+=ordenado[q2]
+        resultado.append([0,a[1],z1])
+        z1+=arreglo[2]
+        q2+=1
+
+        
+        
+        
+    
+    return resultado
+'''
+
+def posicionar(largo,ancho,alto,arreglo,cantidad):
+    q=0
+    x=arreglo[0]
+    y=arreglo[1]
+    z=arreglo[2]
+    A=[None]*cantidad[0]
+    B=[None]*cantidad[1]
+    C=[None]*cantidad[2]
+    D=[None]*cantidad[3]
+    E=[None]*cantidad[4]
+    
+    
+    while z<alto and q<cantidad[0] and y<=ancho:
+        A.append((x,y,z))
+        z+=arreglo[2]
+        q+=1
+
+    y+=arreglo[1]
+    z=0
+    q=0
+   
+
+    while z<alto and q<cantidad[1] and y<=ancho:
+        
+        B.append((x,y,z))
+        z+=arreglo[cantidad[0]*3+2]
+        q+=1
+
+    q=0
+    y+=arreglo[1]
     z=0
 
-    for i in range(3):
+
+    while z<alto and q<cantidad[2]:
+        
+        C.append((x,y,z))
+        z+=arreglo[cantidad[1]*3+2]
+        q+=1
+    while y>ancho and q<cantidad[1]:
+        A.append((x,y,z))
+        B.append((x,y,z))
+        x+=arreglo[0]
+        z+=arreglo[2]
+        q+=1
+
+        
 
 
     
+    resultado=[]
+    resultado.append(A)
+    resultado.append(B)
+    resultado.append(C)
 
+    return resultado
 
+ga=[4,2,6,5,4]
+print(posicionar(20,30,100,resultado,ga))
 
+#print(posicionar(20,30,50,resultado))
+print(ordenado)
+print(resultado)
 
 
 
